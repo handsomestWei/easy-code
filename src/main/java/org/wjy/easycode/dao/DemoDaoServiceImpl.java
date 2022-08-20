78b63b34c812aadfe52ccebeb74f28acf480a2f6
handsomestWei <1293986765@qq.com>
2022-08-20 11:26:00 +0800

    [add] add mybatis-plus lambda demo

--- src/main/java/org/wjy/easycode/dao/DemoDaoServiceImpl.java
+++ src/main/java/org/wjy/easycode/dao/DemoDaoServiceImpl.java
@@ -1,9 +1,11 @@
 package org.wjy.easycode.dao;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.wjy.easycode.pojo.entity.DemoEntity;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 /**
  * mybatis-plus IService使用，以继承的形式扩展功能，提供通用接口
@@ -20,5 +22,11 @@ public class DemoDaoServiceImpl extends ServiceImpl<DemoMapper, DemoEntity> impl
         } else {
             return 0;
         }
+    }
+
+    @Override
+    public DemoEntity selectByName(String name) {
+        // lambda表达式查询，不用显式指定查询字段名称
+        return this.getOne(new LambdaQueryWrapper<DemoEntity>().eq(DemoEntity::getName, name));
     }
 }
--- src/main/java/org/wjy/easycode/dao/IDemoDaoService.java
+++ src/main/java/org/wjy/easycode/dao/IDemoDaoService.java
@@ -1,7 +1,8 @@
 package org.wjy.easycode.dao;
 
-import com.baomidou.mybatisplus.extension.service.IService;
 import org.wjy.easycode.pojo.entity.DemoEntity;
+
+import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
  * IService形式，是BaseMapper的大扩充
@@ -10,4 +11,6 @@ public interface IDemoDaoService extends IService<DemoEntity> {
 
     // 自定义查询，可以封装业务逻辑
     int insertByStatus(Integer status);
+
+    DemoEntity selectByName(String name);
 }
